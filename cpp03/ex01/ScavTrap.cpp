de039d2b99be95f110e97a9a6feecb1bba60d3d0
Michele Sessa <mikysett@gmail.com>
2021-11-09 18:15:33 +0100

    cpp03: ex01 done, 1 bug fixed in ex00

---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,48 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap()
+{
+	name = "lambdaScav";
+	hitpoints = 100;
+	energyPoints = 50;
+	attackDamage = 20;
+	std::cout << "ScavTrap default constructor" << std::endl;
+}
+
+ScavTrap::ScavTrap( std::string name )
+	: ClapTrap(name)
+{
+	hitpoints = 100;
+	energyPoints = 50;
+	attackDamage = 20;
+	std::cout << "ScavTrap " << name << " created" << std::endl;
+}
+
+ScavTrap::ScavTrap( const ScavTrap &oldScavTrap )
+	: ClapTrap(oldScavTrap.name)
+{
+	hitpoints = oldScavTrap.hitpoints;
+	energyPoints = oldScavTrap.energyPoints;
+	attackDamage = oldScavTrap.attackDamage;
+	std::cout << "ScavTrap " << name << " created by copy" << std::endl;
+}
+
+ScavTrap& ScavTrap::operator= ( const ScavTrap &oldScavTrap )
+{
+	name = oldScavTrap.name;
+	hitpoints = oldScavTrap.hitpoints;
+	energyPoints = oldScavTrap.energyPoints;
+	attackDamage = oldScavTrap.attackDamage;
+	std::cout << "ScavTrap " << name << " created with operator=" << std::endl;
+	return (*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << name << " destroyed" << std::endl;
+}
+
+void ScavTrap::guardGate( void )
+{
+	std::cout << "ScavTrap " << name << " have enterred in Gate keeper mode" << std::endl;
+}
